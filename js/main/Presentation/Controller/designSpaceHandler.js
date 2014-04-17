c59e2dc9a30e1027d287098e2d26ab868db2d38e
Juan Carlos Martinez <juancar199400@gmail.com>
2014-04-16 18:26:12 -0600

    Changes Added

---
+++ js/main/Presentation/Controller/designSpaceHandler.js
@@ -0,0 +1,55 @@
+/*!
+ * All Rights Reserved
+ * This software is proprietary information of
+ * Brandon Campos Calderón - Juan Carlos Martínez
+ * Use is subject to license terms.
+ * Filename: presentationNamespace.js
+ */
+
+ /*
+  * Author:      juancar199400@gmail.com
+  * Date:        16/04/2014
+  * Description: Main controller to connect the logic with view
+  */
+
+/*
+ * Global Presentation Layer to be used with MVC Pattern
+ * @namespace
+ */
+(function (pContext, $) {
+	'use strict'
+    /**
+     * Public method to return a reference of handler module.
+     * 
+     * @return {handlerModule} Handler declarations of the MVC.
+     * @public
+     */
+    pContext.getDesignSpaceHandler = function() {
+        return DesignSpaceHandler;
+    };
+
+    /**
+     * Module.
+     *      Controller module.
+     *
+     * @private
+     * @namespace
+     **/
+    var DesignSpaceHandler = (function(){
+        
+        function sentDataToDrawCircle(pPosX, pPosY, pRadius, pFillColor, pStrokeWidth, pStrokeColor){
+            Presentation.getDesignSpace().drawCircle(pPosX, pPosY, pRadius, pFillColor, pStrokeWidth, pStrokeColor);
+        }
+
+        function sentDataToDrawLine(pPosX1, pPosY1, pPosX2, pPosY2, pStrokeWidth, pStrokeColor){
+            Presentation.getDesignSpace().drawLine(pPosX1, pPosY1, pPosX2, pPosY2, pStrokeWidth, pStrokeColor);
+        }
+
+
+        return {
+            sentDataToDrawCircle : sentDataToDrawCircle,
+            sentDataToDrawLine : sentDataToDrawLine    
+        }; 
+    })();    
+
+}(Presentation, jQuery));
